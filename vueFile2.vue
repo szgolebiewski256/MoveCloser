5dd99e738b0d1f34454cc235bb44494e845af557
szgolebiewski256 <60758870+szgolebiewski256@users.noreply.github.com>
2022-05-12 19:41:10 +0200

    Create vueFile2.vue

---
+++ vueFile2.vue
@@ -0,0 +1,45 @@
+<template>
+    <div
+        class="payment-p24"
+    >
+        <div
+            v-for="(payment, index) in paymentsArray"
+            :key="index"
+            class="payment-item"
+            @click="$store.commit('microPayment/toggleP24Token', payment.id)"
+        >
+            <div
+                :class="['payment-item-border', p24Token === payment.id ? 'active' : '' ]"
+            >
+                <img
+                    :src="payment.img"
+                >
+            </div>
+
+            <p>
+                {{ payment.name }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+    name: "P24",
+
+    props: {
+        paymentsArray: {
+            type: Array,
+            default: () => [],
+        },
+    },
+
+    computed: {
+        ...mapGetters({
+            p24Token: "microPayment/getP24Token",
+        }),
+    },
+};
+</script>
